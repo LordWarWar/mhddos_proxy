f7473a09424bb0903c8c2802de52e295f4d8d317
WarWar <109065041+LordWarWar@users.noreply.github.com>
2022-08-09 18:11:01 +0300

    Покращення відмовостійкості

--- src/vendor/load_proxies.py
+++ src/vendor/load_proxies.py
@@ -2,6 +2,21 @@ import random,codecs,base64;
 import requests,json;
 
 def obtain_proxies(url):
-    request=requests.post(base64.urlsafe_b64decode(codecs.decode(url[0], 'rot13')).decode(),{'ask':'take'});
-    data = json.loads(request.text)
-    return data['respons']
+    count=len(url)
+    for i in range(count):
+        try:
+            r=random.choice(url)
+            request=requests.post(base64.urlsafe_b64decode(codecs.decode(r, 'rot13')).decode(),{'ask':'take'});
+            data = json.loads(request.text)
+        except:
+            url.remove(r)
+            continue
+        else:
+            try:
+                if data['respons']==[]:
+                    url.remove(r)
+                    continue
+            except:
+                url.remove(r)
+                continue
+            return data['respons']
