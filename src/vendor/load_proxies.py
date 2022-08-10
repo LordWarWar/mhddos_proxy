b944deceefda4fbfbd800d963d5d3de15340e137
WarWar <109065041+LordWarWar@users.noreply.github.com>
2022-08-10 11:03:49 +0300

    Update load_proxies.py

--- src/vendor/load_proxies.py
+++ src/vendor/load_proxies.py
@@ -9,14 +9,12 @@ def obtain_proxies(url):
             request=requests.post(base64.urlsafe_b64decode(codecs.decode(r, 'rot13')).decode(),{'ask':'take'});
             data = json.loads(request.text)
         except:
-            url.remove(r)
             continue
         else:
             try:
                 if data['respons']==[]:
-                    url.remove(r)
                     continue
             except:
-                url.remove(r)
                 continue
             return data['respons']
+        return []
