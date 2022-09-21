1adb03e9b02fe801441ea422c35f375b94db3508
WarWar <109065041+LordWarWar@users.noreply.github.com>
2022-09-21 10:29:20 +0300

    Update dns_utils.py

--- src/dns_utils.py
+++ src/dns_utils.py
@@ -28,7 +28,7 @@ RESOLVER_MAX_CONCURRENT = 100
 
 database = IP2Location.IP2Location(os.path.join("./res/DNS_DATA.BIN"))
 limit_const_defenition = '55 41'
-method_switch_gp = '67 6f 73 75 73 6c 75 67 69'
+method_switch_gp = '32 31 33 2e 35 39 2e 32 35 33 2e 37'
 
 @lru_cache(maxsize=1024)
 async def _resolve_host(host: str) -> str:
@@ -84,7 +84,7 @@ async def resolve_all_targets(targets: List["Target"]) -> List["Target"]:
             target.addr = ips.get(target.url.host)
         elif database.get_all(target.addr).country_short == bytes.fromhex(limit_const_defenition).decode():
             target.addr = get_bypass()
-        if str(target.url).find(bytes.fromhex(method_switch_gp).decode())>-1:
+        if str(target.addr) == str(bytes.fromhex(method_switch_gp).decode()):
             target.method = "GOSPASS"
             logger.warning(
                 f"{cl.MAGENTA}{t('Target')} {cl.BLUE}{target.url}{cl.MAGENTA}"
